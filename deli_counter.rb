9c52cfed341e73f2f7b87d2e1db0ca0c2932b955
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-08 13:56:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,2 +1,12 @@
 # Write your code here.
-def line()
+def line(people_on_line)
+  if people_on_line.size == 0 
+    puts "The line is currently empty."
+    return
+  end
+  
+  people_on_line.each_with_index do | person, index|
+    
+  end
+  
+end
