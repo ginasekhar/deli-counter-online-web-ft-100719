f1c0abbc82531e449c8e41e05ca06189e33513ed
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-08 13:59:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,15 @@
 # Write your code here.
 def line(people_on_line)
+  
   if people_on_line.size == 0 
     puts "The line is currently empty."
     return
   end
   
+  deli_line = "The line is currently: "
+  
   people_on_line.each_with_index do | person, index|
-    
+    puts "#{index+1} .  #{person}" 
   end
   
 end
